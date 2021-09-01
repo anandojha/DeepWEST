23a76f3c33717f9681d1cdbe4fc12ddd122593af
anandojha <anandojha.002@gmail.com>
2021-09-01 13:51:26 -0400

    Added netcdf to pdb function

--- DeepWEST/DeepWEST.py
+++ DeepWEST/DeepWEST.py
@@ -20,6 +20,79 @@ import re
 ################ Amber Trjaectory to array ################
 
 
+def get_alad_traj_pdb_from_nc(
+    traj,
+    ref_pdb="reference.pdb",
+    start=0,
+    stop=250000,
+    stride=1,
+    traj_pdb="alad_multi.pdb",
+):
+
+    """
+    Converts NETCDF trajectry data to a multiPDB trajectory data
+    """
+    command = "curl -O http://ftp.imp.fu-berlin.de/pub/cmb-data/alanine-dipeptide-nowater.pdb"
+    os.system(command)
+    command = "mv alanine-dipeptide-nowater.pdb " + ref_pdb
+    os.system(command)
+    topology = md.load(ref_pdb).topology
+    print(topology)
+    trajec = md.load(traj, top=ref_pdb)
+    trajec = trajec[start:stop:stride]
+    print(trajec)
+    trajec.save_pdb(traj_pdb, force_overwrite=True)
+    command = "rm -rf " + ref_pdb
+    os.system(command)
+    fin = open(traj_pdb, "rt")
+    fout = open("intermediate_I.txt", "wt")
+    for line in fin:
+        fout.write(line.replace("ENDMDL", "END"))
+    fin.close()
+    fout.close()
+    file1 = open("intermediate_I.txt", "r")
+    file2 = open("intermediate_II.txt", "w")
+    for line in file1.readlines():
+        if not (line.startswith("TER")):
+            file2.write(line)
+    file2.close()
+    file1.close()
+    file1 = open("intermediate_II.txt", "r")
+    file2 = open("intermediate_III.txt", "w")
+    for line in file1.readlines():
+        if not (line.startswith("REMARK")):
+            file2.write(line)
+    file2.close()
+    file1.close()
+    file1 = open("intermediate_III.txt", "r")
+    file2 = open("intermediate_IV.txt", "w")
+    for line in file1.readlines():
+        if not (line.startswith("MODEL")):
+            file2.write(line)
+    file2.close()
+    file1.close()
+    file1 = open("intermediate_IV.txt", "r")
+    file2 = open("intermediate_V.txt", "w")
+    for line in file1.readlines():
+        if not (line.startswith("CRYST")):
+            file2.write(line)
+    file2.close()
+    file1.close()
+    file1 = open("intermediate_V.txt", "r")
+    file2 = open("intermediate_VI.txt", "w")
+    for line in file1.readlines():
+        if not (line.startswith("CONECT")):
+            file2.write(line)
+    file2.close()
+    file1.close()
+    with open("intermediate_VI.txt") as f1:
+        lines = f1.readlines()
+    with open(traj_pdb, "w") as f2:
+        f2.writelines(lines[:-1])
+    command = "rm -rf intermediate_I.txt intermediate_II.txt intermediate_III.txt intermediate_IV.txt intermediate_V.txt intermediate_VI.txt"
+    os.system(command)
+
+
 def create_heavy_atom_xyz(
     traj, ref_pdb, heavy_atoms_array, start=0, stop=500000, stride=1
 ):
